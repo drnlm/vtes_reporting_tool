d4d481d96c57bc8a50660e26f3c9c22aba3ceeea
Neil <neil@dip.sun.ac.za>
2013-09-11 17:05:07 +0200

    Add swapping in game reporting widget

--- vtesgame.py
+++ vtesgame.py
@@ -13,16 +13,58 @@ from kivy.uix.widget import Widget
 from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
 from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
 
 
-class GameWidget(Widget):
+class GameReportWidget(Widget):
 
-    grid = ObjectProperty()
+    game = ObjectProperty(None)
+    players = ObjectProperty(None)
 
-    def __init__(self, **kwargs):
-        super(GameWidget, self).__init__(**kwargs)
-        self.aPlayers = []
+    def __init__(self, oParent):
+        super(GameReportWidget, self).__init__()
+        self.oParent = oParent
+        self.iCur = 0
+        self.aPlayer = []
         self.dDecks = {}
+        self.aLog = []
+
+    def set_players(self, aPlayers):
+        self.aPlayers = aPlayers
+
+    def set_decks(self, dDecks):
+        self.dDecks = dDecks
+        y_size = 120 // len(self.aPlayers)
+        for oWidget in self.players.children[:]:
+            self.players.remove_widget(oWidget)
+        for sPlayer in self.aPlayers:
+            if sPlayer in self.dDecks:
+                label = Label(text='%s (%s) bleeding' % (
+                    sPlayer, self.dDecks[sPlayer]), size_hint_y=y_size)
+            else:
+                label = Label(text='%s bleeding' % sPlayer,
+                              size_hint_y=y_size)
+            self.players.add_widget(label)
+
+    def next(self):
+        self.iCur += 1
+        if self.iCur >= len(self.aPlayers):
+            self.iCur = 0
+
+    def oust(self):
+        pass
+
+    def stop_game(self):
+        self.oParent.stop_game()
+
+
+class PlayerSelectWidget(Widget):
+
+    grid = ObjectProperty(None)
+
+    def __init__(self, oParent):
+        super(PlayerSelectWidget, self).__init__()
+        self.oParent = oParent
         self._aNameWidgets = []
         self._aDeckWidgets = []
         for i in range(1, 8):
@@ -36,22 +78,38 @@ class GameWidget(Widget):
             self._aDeckWidgets.append(oTextInput)
 
     def start(self):
+        aPlayers = []
+        dDecks = {}
         for oPlayer, oDeck in zip(self._aNameWidgets, self._aDeckWidgets):
             if oPlayer.text.strip():
-                self.aPlayers.append(oPlayer.text.strip())
+                aPlayers.append(oPlayer.text.strip())
                 if oDeck.text.strip():
-                    self.dDecks[oPlayer.text.strip()] = oDeck.text.strip()
-        print self.aPlayers, self.dDecks
+                    dDecks[oPlayer.text.strip()] = oDeck.text.strip()
+        self.parent.start_game(aPlayers, dDecks)
 
 
+class GameWidget(BoxLayout):
+
+    def __init__(self):
+        super(GameWidget, self).__init__()
+        self.select = PlayerSelectWidget(self)
+        self.game = GameReportWidget(self)
+        self.add_widget(self.select)
+
+    def start_game(self, aPlayers, dDecks):
+        self.remove_widget(self.select)
+        self.game.set_players(aPlayers)
+        self.game.set_decks(dDecks)
+        self.add_widget(self.game)
+
+    def stop_game(self):
+        self.remove_widget(self.game)
+        self.add_widget(self.select)
 
 
 class VTESGameApp(App):
 
    title = "VTES Game Reporter"
-
-   def __init__(self):
-       super(VTESGameApp, self).__init__()
 
    def build(self):
        return GameWidget()
